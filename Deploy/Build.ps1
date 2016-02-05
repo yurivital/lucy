cd0bbe398c02c3ff9a552eb9e3a906f5cb91b34b
yuri vital <vital.systm@gmail.com>
2016-02-05 07:49:24 +0100

    build alpha

--- Deploy/Build.ps1
+++ Deploy/Build.ps1
@@ -1,7 +1,7 @@
 $msBuildCmd = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 $solution =  "Lucy.sln"
 $destination = 'deploy_tmp'
-
+$nsisScript ='deploy\lucy_alpha.nsi'
 
 & $msBuildCmd  @($solution,  "/t:Rebuild", "/p:Configuration=Release" , "/consoleloggerparameters:summary")
 
@@ -15,4 +15,4 @@ else
 
 ls  "Lucy.Client.Desktop\bin\Release\" -Recurse -Exclude "*.pdb" | % { Copy-Item $_.FullName  $destination }
 
-& 'C:\Program Files (x86)\NSIS\makensis.exe' @("deploy\lucy_1.0.nsi")
+& 'C:\Program Files (x86)\NSIS\makensis.exe' @($nsisScript)
